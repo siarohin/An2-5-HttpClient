a191c95b18f72a8be51c6921be1f88b3896f4666
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-10-01 16:13:04 +0300

    update project and docs

--- src/app/tasks/components/task-form/task-form.component.ts
+++ src/app/tasks/components/task-form/task-form.component.ts
@@ -25,17 +25,21 @@ export class TaskFormComponent implements OnInit {
 
     // it is not necessary to save subscription to route.paramMap
     // when router destroys this component, it handles subscriptions automatically
+    const observer = {
+      next: (task: TaskModel) => (this.task = { ...task }),
+      error: (err: any) => console.log(err)
+    };
     this.route.paramMap
       .pipe(
         switchMap((params: ParamMap) =>
           this.taskArrayService.getTask(+params.get('taskID'))
         )
       )
-      .subscribe(task => (this.task = { ...task }), err => console.log(err));
+      .subscribe(observer);
   }
 
   onSaveTask() {
-    const task = { ...this.task };
+    const task = { ...this.task } as TaskModel;
 
     if (task.id) {
       this.taskArrayService.updateTask(task);
--- src/app/users/components/user-list/user-list.component.ts
+++ src/app/users/components/user-list/user-list.component.ts
@@ -27,21 +27,22 @@ export class UserListComponent implements OnInit {
     this.users$ = this.userArrayService.getUsers();
 
     // listen editedUserID from UserFormComponent
+    const observer = {
+      next: (user: UserModel) => {
+        this.editedUser = { ...user };
+        console.log(
+          `Last time you edited user ${JSON.stringify(this.editedUser)}`
+        );
+      },
+      error: (err: any) => console.log(err)
+    };
     this.route.paramMap
       .pipe(
         switchMap((params: ParamMap) =>
           this.userArrayService.getUser(+params.get('editedUserID'))
         )
       )
-      .subscribe(
-        (user: UserModel) => {
-          this.editedUser = { ...user };
-          console.log(
-            `Last time you edited user ${JSON.stringify(this.editedUser)}`
-          );
-        },
-        err => console.log(err)
-      );
+      .subscribe(observer);
   }
 
   onEditUser(user: UserModel) {
